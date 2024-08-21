1a3cc0979cafc68c3c94017aa1a512563cbfa0aa
kennyg37 <k.ganza@alustudent.com>
2024-08-21 21:50:32 +0200

    returning the first non repeating integer challenge

--- Codewars/non_repeating.py
+++ Codewars/non_repeating.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+def first_non_repeating_letter(string):
+    lower_string = string.lower()
+    for index, char in enumerate(lower_string):
+        if lower_string.count(char) == 1:
+            return string[index]
+    return ''
