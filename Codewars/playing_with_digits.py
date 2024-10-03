c0791aa878729a1200fada59550472ab392ca216
kennyg37 <k.ganza@alustudent.com>
2024-10-03 10:45:21 +0200

    reducing complexity and redundancy

--- Codewars/playing_with_digits.py
+++ Codewars/playing_with_digits.py
@@ -1,21 +1,15 @@
 def dig_pow(n, p):
     num_list = []
     for index, num in enumerate(str(n)):
-        print(str(n))
         new = int(num)**p
-        num_list.append(new)
         p += 1
-    print(num_list)
-        
-    for i in num_list:
-        int_sum = sum(num_list)
-        print(int_sum)
-        if int_sum % n == 0:
-            k = (int_sum/n)
-            k = round(k)
-            return k
-        else:
-            return -1
+        num_list.append(new)
+    int_sum = sum(num_list)
+    if int_sum % n == 0:
+        k = round(int_sum/n)
+        return k
+    else:
+        return -1
         
         
 print(dig_pow(46288, 3))
